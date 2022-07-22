a6f6bf69a20317e48f6449b00a8cc5e033b3f669
Ranjan <ranjanghimire862@gmail.com>
2022-07-22 14:54:04 +0545

    Firebase  iitialized

--- src/utils/firebase/firbase.utils.js
+++ src/utils/firebase/firbase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth , signInWithRedirect , signInWithPopup , GoogleAuthProvider } from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBGKpBiZgFhxalCX6pJluBrR8iaHu7wIVk",
@@ -21,3 +22,10 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth,provider);
+  export const db = getFirestore();
+
+
+  const createUserDocumentFromAuth = async (userAuth) =>{
+    const userDocRef = doc(db, 'users', userAuth.uid)
+    const userSnapshot = await getDoc(userDocRef);
+  }
