4c714262cde2c3ea9a482321a5f74b8731ef3686
Ranjan <ranjanghimire862@gmail.com>
2022-07-22 17:18:38 +0545

    firebase 1

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,11 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firbase.utils";
+import { signInWithGooglePopup , createUserDocumentFromAuth } from "../../utils/firebase/firbase.utils";
 
 const SignIn = () =>{
     const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-        console.log(response);
-    }
+        const {user} = await signInWithGooglePopup();
+        const userDocRef= await createUserDocumentFromAuth(user);
+    };
+
     return(
         <div>
             <h1>
--- src/utils/firebase/firbase.utils.js
+++ src/utils/firebase/firbase.utils.js
@@ -25,7 +25,21 @@ const firebaseConfig = {
   export const db = getFirestore();
 
 
-  const createUserDocumentFromAuth = async (userAuth) =>{
+ export const createUserDocumentFromAuth = async (userAuth) =>{
     const userDocRef = doc(db, 'users', userAuth.uid)
     const userSnapshot = await getDoc(userDocRef);
+
+
+    if (!userSnapshot.exists()){
+      const{displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await setDoc( userDocRef , {displayName, email, createdAt});
+      }
+      catch(error){
+        console.log('error creating user', error.message);
+      }
+    }
+    return userDocRef;
   }
