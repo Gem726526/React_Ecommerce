4a713d277fb067f8520988102cf1d47eea68ab2c
Ranjan <ranjanghimire862@gmail.com>
2022-07-22 13:41:40 +0545

    Firebase  iitialized

---
+++ src/utils/firebase/firbase.utils.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth , signInWithRedirect , signInWithPopup , GoogleAuthProvider } from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBGKpBiZgFhxalCX6pJluBrR8iaHu7wIVk",
+    authDomain: "react-ecommerce-a1c39.firebaseapp.com",
+    projectId: "react-ecommerce-a1c39",
+    storageBucket: "react-ecommerce-a1c39.appspot.com",
+    messagingSenderId: "145915520480",
+    appId: "1:145915520480:web:b454970279edee57667785"
+  };
+  
+
+  const firebaseapp = initializeApp(firebaseConfig);
+
+
+  const provider = new GoogleAuthProvider();
+  provider.setCustomParameters({
+    prompt: "select_account"
+  });
+
+  export const auth = getAuth();
+  export const signInWithGooglePopup = () => signInWithPopup(auth,provider);
